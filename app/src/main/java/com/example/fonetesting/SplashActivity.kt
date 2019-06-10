d217f54e3b6869209dbdc99fc2867c00fe2adaf5
f-molina <00300917@uca.edu.sv>
2019-06-09 18:36:42 -0600

    primeras pantallas y diseno

---
+++ app/src/main/java/com/example/fonetesting/SplashActivity.kt
@@ -0,0 +1,27 @@
+package com.example.fonetesting
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import java.lang.Exception
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_activity)
+
+        val background = object : Thread(){
+            override fun run() {
+                try {
+                    Thread.sleep(5000)
+                    val intent = Intent(baseContext, MainActivity::class.java)
+                    startActivity(intent)
+                } catch (e: Exception){
+                    e.printStackTrace()
+                }
+            }
+        }
+        background.start()
+    }
+}
