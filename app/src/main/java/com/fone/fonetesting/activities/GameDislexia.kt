b0b20e6258dda8ec0f14c7e454510f7e084ef113
waba10 <00189617@uca.edu.sv>
2019-06-26 12:33:17 -0600

    adding new game

---
+++ app/src/main/java/com/fone/fonetesting/activities/GameDislexia.kt
@@ -0,0 +1,13 @@
+package com.fone.fonetesting.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fone.fonetesting.R
+
+class GameDislexia : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game_dislexia)
+    }
+}
