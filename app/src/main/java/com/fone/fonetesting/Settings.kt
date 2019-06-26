bc97da4a6a32a6d98d2346091117998c12ed9a7b
Raul Granados <00138816@uca.edu.sv>
2019-06-26 14:15:50 -0600

    Agregando clase de Settings

---
+++ app/src/main/java/com/fone/fonetesting/Settings.kt
@@ -0,0 +1,13 @@
+package com.fone.fonetesting
+
+class Settings {
+    companion object{
+        var pitch: Float
+        var speed: Float
+
+        init {
+            pitch = 1.0f
+            speed = 1.0f
+        }
+    }
+}
